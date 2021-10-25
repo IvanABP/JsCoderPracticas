cc1077cb281fca15212b4d7cda700fca87b1ffa2
IvanABP <i-balestrieri@outlook.com>
2021-10-25 14:16:34 -0300

    Subida de la segunda actividad

--- clase-1/actividad 1/actividad 2/main.js
+++ clase-1/actividad 1/actividad 2/main.js
@@ -1,3 +1,14 @@
 /* 
 Crear cinco (5) variables constantes, asignando a cada una de ellas diferentes nombres de ciudades. Generalmente, dichas variables se declaran con const.
 */
+const ciudad1 = "Springfield";
+const ciudad2 = "Shelbyville";
+const ciudad3 = "Capital City";
+const ciudad4 = "Bronson, Missouri";
+const ciudad5 = "Albuquerque";
+
+console.log(ciudad1);
+console.log(ciudad2);
+console.log(ciudad3);
+console.log(ciudad4);
+console.log(ciudad5);
